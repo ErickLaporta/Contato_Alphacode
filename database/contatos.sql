3665875445f705d6418e3eee7a16892150029cb5
Erick Laporta <erickyanlaportadasilvacontas@gmail.com>
2025-07-15 23:41:57 -0300

    Feat: Criando banco de dados.

---
+++ database/contatos.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS contatos_db ;
+
+USE contatos_db;
+
+CREATE TABLE IF NOT EXISTS contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    celular VARCHAR(20) NOT NULL,
+    profissao VARCHAR(255) NOT NULL,
+    nascimento DATE NOT NULL,
+    whatsapp BOOLEAN NOT NULL DEFAULT 0,
+    sms BOOLEAN NOT NULL DEFAULT 0,
+    notificarEmail BOOLEAN NOT NULL DEFAULT 0
+);
