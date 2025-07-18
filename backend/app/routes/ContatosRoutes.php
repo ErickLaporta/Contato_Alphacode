313052375e80988006428bd8dbdcf90fa6697bbf
Erick Laporta <erickyanlaportadasilvacontas@gmail.com>
2025-07-17 23:42:19 -0300

    feat: Adicionando header em ContatosRoutes.

--- backend/app/routes/ContatosRoutes.php
+++ backend/app/routes/ContatosRoutes.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 require_once __DIR__ . '/../controllers/ContatosController.php';
 
