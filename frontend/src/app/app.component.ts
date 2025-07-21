6008b7c6d444993ebfb892fe2516c201800cf390
Erick Laporta <erickyanlaportadasilvacontas@gmail.com>
2025-07-20 23:32:29 -0300

    feat: Configurando arquivos app e mudando a estilização

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ContatosComponent } from '../pages/contatos/contatos.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ContatosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { TableComponent } from '../components/table/table.component';
+import { HomeComponent } from '../components/home/home.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/cadastro', pathMatch: 'full' },
+  { path: 'cadastro', component: HomeComponent },
+  { path: 'contatos', component: TableComponent },
+  { path: '**', redirectTo: '/cadastro' }
+];
