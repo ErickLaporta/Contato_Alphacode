cda6588183bb310aa2574cea907cfab16684b281
Erick Laporta <erickyanlaportadasilvacontas@gmail.com>
2025-07-20 23:34:26 -0300

    feat: Criando camponente table.

---
+++ frontend/src/components/table/table.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Contato } from '../../../types/interfaces';
+import { ContatosService } from './../../services/contatos.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-table',
+  imports: [CommonModule],
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+  contatos: Contato[] = [];
+
+  constructor(private contatosService: ContatosService, private contatoService: ContatosService) { }
+
+  ngOnInit() {
+    this.contatosService.listarContatos().subscribe((contatos: Contato[]) => {
+      this.contatos = contatos;
+    });
+  }
+
+  excluirContato(id?: number) {
+    if (id) {
+      this.contatosService.excluirContato(id).subscribe(() => {
+        this.contatos = this.contatos.filter(contato => contato.id !== id);
+      });
+    }
+  }
+
+  editarContato(contato: Contato) {
+    this.contatoService.salvarContato(contato);
+  }
+}
