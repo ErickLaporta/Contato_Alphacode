e2c8ae9bd5dd7f7ad3f5b8b58d43bec1da26d351
Erick Laporta <erickyanlaportadasilvacontas@gmail.com>
2025-07-21 13:16:04 -0300

    refactor: Ajustando ContatosController

--- backend/app/controllers/ContatosController.php
+++ backend/app/controllers/ContatosController.php
@@ -11,17 +11,17 @@ class ContatosController {
 
     public function validarDados($dados) {
         switch (true) {
-            case ($dados["nome"] === "" || strlen($dados["nome"]) < 3):
+            case (empty($dados["nome"]) || strlen($dados["nome"]) < 3):
                 return "O nome deve ter pelo menos 3 caracteres.";
-            case ($dados["email"] === "" || !filter_var($dados["email"], FILTER_VALIDATE_EMAIL)):
+            case (empty($dados["email"]) || !filter_var($dados["email"], FILTER_VALIDATE_EMAIL)):
                 return "O e-mail é inválido.";
-            case ($dados["telefone"] === "" || strlen($dados["telefone"]) !== 10):
+            case (empty($dados["telefone"]) || strlen($dados["telefone"]) !== 10):
                 return "O telefone deve conter 10 dígitos.";
-            case ($dados["celular"] === "" || strlen($dados["celular"]) !== 11):
+            case (empty($dados["celular"]) || strlen($dados["celular"]) !== 11):
                 return "O celular deve conter 11 dígitos.";
-            case ($dados["profissao"] === "" || strlen($dados["profissao"]) < 3):
+            case (empty($dados["profissao"]) || strlen($dados["profissao"]) < 3):
                 return "A profissão deve ter pelo menos 3 caracteres.";
-            case ($dados["nascimento"] === ""):
+            case (empty($dados["nascimento"])):
                 return "A data de nascimento deve conter dia, mês e ano por extenso.";
             case !is_numeric($dados["whatsapp"]) || !is_numeric($dados["sms"]) || !is_numeric($dados["notificarEmail"]):
                 return "Os campos WhatsApp, SMS e Notificar Email devem ser 0 ou 1.";
@@ -30,11 +30,13 @@ class ContatosController {
         }
     }
 
-    public function cadastrarContato($dados) {        
+    public function cadastrarContato($dados) {
         return $this->contatoModel->cadastrarContato($dados);
     }
 
     public function editarContato($id, $dados) {
+        $dados['telefone'] = preg_replace('/\D/', '', $dados['telefone']);
+        $dados['celular'] = preg_replace('/\D/', '', $dados['celular']);
         $erro = $this->validarDados($dados);
         if ($erro) return $erro;
 
